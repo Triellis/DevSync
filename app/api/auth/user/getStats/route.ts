3dc707e5d6950979bc73c4a8ade34ed3cf224258
Saumya <saumyabshah90@gmail.com>
2024-03-02 20:02:04 +0530

    ghub username in cookies

--- app/api/auth/user/getStats/route.ts
+++ app/api/auth/user/getStats/route.ts
@@ -1,9 +1,5 @@
-import connectToDB from "@/lib/DB";
 import { NextRequest, NextResponse } from "next/server";
-import User from "@/lib/Schemas/User";
 
 export const GET = async (req: NextRequest, res: NextResponse) => {
-const {username} = req.query;
-
-
+    const username = req.cookies?.get('username');
 }
--- lib/function.ts
+++ lib/function.ts
@@ -1,7 +1,11 @@
+import { cookies } from "next/headers";
+
 export const getGihubBasicInfo = async (userId:string) => {
     try {
         const res = await fetch(`https://api.github.com/user/${userId}`);
         const data = await res.json();
+
+        cookies().set('username', data.login);
 
         return {
             githubName: data.login,
