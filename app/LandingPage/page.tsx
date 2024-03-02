e7f9f84d80a703fde03569e2bc19cadac7aafc50
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-03-02 16:57:59 +0530

    Refactor import paths and remove unused Logo component

--- app/LandingPage/page.tsx
+++ app/LandingPage/page.tsx
@@ -1,8 +1,8 @@
 import { authConfig } from "@/lib/Auth/auth";
 import { getServerSession } from "next-auth";
-import LoginBtn from "../components/LoginBtn";
-import Logo from "../components/Logo";
-import SignoutBtn from "../components/Temp/SignoutBtn";
+import LoginBtn from "@/components/LoginBtn";
+import Logo from "../../components/Logo";
+import SignoutBtn from "@/components/Temp/SignoutBtn";
 import styles from "./LandingPage.module.css";
 
 export default async function Home() {
