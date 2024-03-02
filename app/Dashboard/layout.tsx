716391bee6a930e4050a4378de18b09959da527b
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2024-03-02 17:13:41 +0530

    Delete layout and page components, update Navbar styles

--- app/Dashboard/layout.tsx
+++ app/Dashboard/layout.tsx
@@ -1,7 +1,7 @@
+import styles from "@/app/styles/page.module.css";
+import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import { Cabin } from "next/font/google";
-
-import Navbar from "@/components/Navbar";
 
 export default function RootLayout({
 	children,
@@ -10,8 +10,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<div>
-			<Navbar />
-			{children}
+			<div className={styles.nav}>
+				<Navbar />
+			</div>
+			<div>{children}</div>
 		</div>
 	);
 }
