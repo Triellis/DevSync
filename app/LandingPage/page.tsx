b633e13e45eff2c7a78f607a36e78b596971dda2
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2024-03-03 02:27:33 +0530

    reposition the block on landing page

--- app/LandingPage/page.tsx
+++ app/LandingPage/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import LoginBtn from "@/components/LoginBtn";
+import { useSession } from "next-auth/react";
+import { redirect, useRouter } from "next/navigation";
 import Logo from "../../components/Logo";
 import styles from "./LandingPage.module.css";
-import { redirect, useRouter } from "next/navigation";
-import { useSession } from "next-auth/react";
 
 export default function Home() {
 	const { data: session, status } = useSession();
@@ -15,10 +15,12 @@ export default function Home() {
 
 	return (
 		<div className={styles.main}>
-			<Logo fSize="84px" />
+			<div className={styles.block}>
+				<Logo fSize="84px" />
 
-			<h1 className={styles.title}>Sync In. Stand Out.</h1>
-			<LoginBtn />
+				<h1 className={styles.title}>Sync In. Stand Out.</h1>
+				<LoginBtn />
+			</div>
 		</div>
 	);
 }
