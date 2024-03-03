d9a3259a131e2da2ff67fa0266db8a085730cfac
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2024-03-03 09:17:31 +0530

    update social link

--- components/SocialLinks/SocialLinks.tsx
+++ components/SocialLinks/SocialLinks.tsx
@@ -7,7 +7,7 @@ import { Flex } from "@chakra-ui/react";
 export default function SocialLinks() {
 	return (
 		<Flex direction={"column"} gap={8}>
-			<a href="https://github.com/JeelRajodiya" target="_blank">
+			<a href="https://github.com/Triellis/DevSync" target="_blank">
 				<Flex gap={4} alignItems={"center"}>
 					<GHIcon /> GitHub
 				</Flex>
