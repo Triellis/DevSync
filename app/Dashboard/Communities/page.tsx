f2ca9aceec4d36c5313a3a2631c68d384cbfe5ae
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-03-03 05:46:48 +0530

    Update CommunityCard component with new data

--- app/Dashboard/Communities/page.tsx
+++ app/Dashboard/Communities/page.tsx
@@ -7,6 +7,8 @@ function Communities() {
 		<div className={styles.main}>
 			<div className={styles.wrap}>
 				<CommunityCard
+					name="Json Schema"
+					followers={1264}
 					pic={
 						"https://avatars.githubusercontent.com/u/13019229?s=48&v=4"
 					}
@@ -15,22 +17,18 @@ function Communities() {
 					}
 				/>
 				<CommunityCard
+					name="AsyncAPI Initiative"
+					followers={785}
 					pic={
-						"https://avatars.githubusercontent.com/u/13019229?s=48&v=4"
+						"https://avatars.githubusercontent.com/u/16401334?s=200&v=4"
 					}
 					desc={
-						"The organisation responsible for the Json Schema specification"
+						"AsyncAPI seeks to improve the current state of Event-Driven Architectures (EDAs) and the tooling system around them. "
 					}
 				/>
 				<CommunityCard
-					pic={
-						"https://avatars.githubusercontent.com/u/13019229?s=48&v=4"
-					}
-					desc={
-						"The organisation responsible for the Json Schema specification"
-					}
-				/>
-				<CommunityCard
+					name="Json Schema"
+					followers={1264}
 					pic={
 						"https://avatars.githubusercontent.com/u/13019229?s=48&v=4"
 					}
