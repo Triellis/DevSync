98bb2c8f8654254b5e4f3111021d9fef9e8d2682
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-03-03 05:54:04 +0530

    Update Communities module CSS and CommunityCard component

--- app/Dashboard/Communities/page.tsx
+++ app/Dashboard/Communities/page.tsx
@@ -27,13 +27,39 @@ function Communities() {
 					}
 				/>
 				<CommunityCard
-					name="Json Schema"
-					followers={1264}
+					name="Google"
+					followers={21301}
 					pic={
-						"https://avatars.githubusercontent.com/u/13019229?s=48&v=4"
+						"https://avatars.githubusercontent.com/u/1342004?s=200&v=4"
+					}
+					desc={"Google ❤️ Open Source"}
+				/>
+				<CommunityCard
+					name="Meta"
+					followers={21301}
+					pic={
+						"https://avatars.githubusercontent.com/u/69631?s=200&v=4"
 					}
 					desc={
-						"The organisation responsible for the Json Schema specification"
+						"We are working to build community through open source technology. NB: members must have two-factor auth."
+					}
+				/>
+				<CommunityCard
+					name="Python Software Foundation"
+					followers={1301}
+					pic={
+						"https://avatars.githubusercontent.com/u/50630501?s=200&v=4"
+					}
+					desc={"Python. It's easy. "}
+				/>
+				<CommunityCard
+					name="Meta"
+					followers={21301}
+					pic={
+						"https://avatars.githubusercontent.com/u/69631?s=200&v=4"
+					}
+					desc={
+						"We are working to build community through open source technology. NB: members must have two-factor auth."
 					}
 				/>
 			</div>
